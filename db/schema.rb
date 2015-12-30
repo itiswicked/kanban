597d45d446175cc1007f3ac567f564175c211b07
Jonathan Devers <jonathan.devers@gmail.com>
2015-12-30 14:50:54 -0500

    add popdown

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230170810) do
+ActiveRecord::Schema.define(version: 20151230173320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20151230170810) do
   create_table "projects", force: :cascade do |t|
     t.integer  "user_id",      null: false
     t.string   "project_name", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer  "project_id", null: false
+    t.string   "task_name",  null: false
+    t.integer  "phase",      null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
